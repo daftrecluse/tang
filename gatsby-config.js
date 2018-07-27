ec04f12b6d5da4aac58169f8ba4d312e0264c7b5
Rakesh Verma <rakeshverma@Rakeshs-MacBook-Pro.local>
2018-07-27 13:17:23 +0530

    missing files

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Tang UI',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Tang UI Design System</h1>
+    <p>The design system, guidelines and pattern library at Acko.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
